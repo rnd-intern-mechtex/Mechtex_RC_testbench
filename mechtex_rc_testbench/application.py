3d0f8a0e60a43356df79a7b3a96d11c50e7f4cf3
Intern Mechtex <intern.mechtex@gmail.com>
2022-06-10 14:03:46 +0530

    better init

--- mechtex_rc_testbench/application.py
+++ mechtex_rc_testbench/application.py
@@ -1,11 +1,11 @@
+from threading import Thread
+from time import perf_counter, sleep
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from serial.tools.list_ports import comports
-from threading import Thread
-from . import views as v
-from . import models as m
-from time import sleep, perf_counter
+from mechtex_rc_testbench.views import AutomatedPage, ManualPage, SetupPage
+from mechtex_rc_testbench.models import Arduino, Model, PowerSupply
 
 class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -24,107 +24,87 @@ class Application(tk.Tk):
         self.container.pack(fill='both', side='top', expand=True)
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)        
+        
+        # -------------------------------------------------------------------------------
+        # CREATING ALL VIEWS
+        # -------------------------------------------------------------------------------
         self.frames = {}
-        
+        self.frames['setup'] = SetupPage(self.container, self)
+        self.frames['manual'] = ManualPage(self.container, self)
+        self.frames['auto'] = AutomatedPage(self.container, self)
+
         # -------------------------------------------------------------------------------
-        # Creating all the views
-        # -------------------------------------------------------------------------------
-        self.frames["setup"] = v.SetupPage(self.container, self)
-        self.frames["manual"] = v.ManualPage(self.container, self)
-        self.frames["auto"] = v.AutomatedPage(self.container, self)
-        
-        # -------------------------------------------------------------------------------
-        # SETUP VIEW INIT
+        # SETUP VIEW WIDGETS CONFIGURATION
         # -------------------------------------------------------------------------------
         
-        self.setup__on_refresh()    # refresh comports on start
-        self.frames["setup"].refresh_button.config(command=self.setup__on_refresh)
-        self.frames["setup"].source_button.config(command=self.setup__on_browse_src)
-        self.frames["setup"].destination_button.config(command=self.setup__on_browse_dest)
-        self.frames["setup"].button_save.config(command=self.setup__on_save)
-        self.frames["setup"].button_auto.config(
-            state=tk.DISABLED,
-            command=self.setup__on_auto
-        )
-        self.frames["setup"].button_manual.config(
-            state=tk.DISABLED,
-            command=self.setup__on_manual
-        )
-        
+        self.setup__on_refresh()
+        self.frames['setup'].refresh_button.config(command=self.setup__on_refresh)
+        self.frames['setup'].source_button.config(command=self.setup__on_browse_src)
+        self.frames['setup'].destination_button.config(command=self.setup__on_browse_dest)
+        self.frames['setup'].button_save.config(command=self.setup__on_save)
+        self.frames['setup'].button_auto.config(command=self.setup__on_auto)
+        self.frames['setup'].button_manual.config(command=self.setup__on_manual)
+
         # -------------------------------------------------------------------------------
-        # MANUAL VIEW INIT
+        # MANUAL VIEW WIDGETS CONFIGURATION
         # -------------------------------------------------------------------------------
-        
         self.slider_voltage = tk.DoubleVar()
         self.slider_pwm = tk.DoubleVar()
         self.entry_voltage = tk.DoubleVar()
         self.entry_pwm = tk.DoubleVar()
-        self.old_voltage = 0.0
-        self.old_pwm = 1000
-        self.slider_voltage.set(0)
-        self.slider_pwm.set(1000)
-        for widget in self.frames["manual"].pwm_frame.winfo_children():
-            widget.config(state=tk.DISABLED)
-        for widget in self.frames["manual"].voltage_frame.winfo_children():
-            widget.config(state=tk.DISABLED)
-            
-        # Adding functionalities and init states
-        self.frames["manual"].start_button.config(command=self.manual__on_start)
-        self.frames["manual"].stop_button.config(
-            command=self.manual__on_stop,
-            state=tk.DISABLED
-        )
-        self.frames["manual"].back_button.config(command=self.manual__on_back)
-        self.frames["manual"].voltage_slider.config(
+
+        self.frames['manual'].start_button.config(command=self.manual__on_start)
+        self.frames['manual'].stop_button.config(command=self.manual__on_stop)
+        self.frames['manual'].back_button.config(command=self.manual__on_back)
+
+        self.frames['manual'].voltage_slider.config(
             variable=self.slider_voltage,
             command=self.manual__v_slider_changed
         )
-        self.frames["manual"].pwm_slider.config(
+        self.frames['manual'].pwm_slider.config(
             variable=self.slider_pwm,
             command=self.manual__p_slider_changed
         )
-        for val in self.frames["manual"].dashboard_values:
-            val.config(text = '-')
-        
-        # Binding functions with events
-        self.frames["manual"].voltage_entry.config(textvariable=self.entry_voltage)
-        self.frames["manual"].voltage_entry.bind('<Return>', self.manual__on_v_set)
-        self.frames["manual"].pwm_entry.config(textvariable=self.entry_pwm)
-        self.frames["manual"].pwm_entry.bind('<Return>', self.manual__on_p_set)
-        self.frames["manual"].voltage_button.bind('<Return>', self.manual__on_v_set)
-        self.frames["manual"].voltage_button.bind('<Button-1>', self.manual__on_v_set)
-        self.frames["manual"].pwm_button.bind('<Return>', self.manual__on_p_set)
-        self.frames["manual"].pwm_button.bind('<Button-1>', self.manual__on_p_set)
+        self.frames['manual'].voltage_entry.config(textvariable=self.entry_voltage)
+        self.frames['manual'].voltage_entry.bind('<Return>', self.manual__on_v_set)
+        self.frames['manual'].pwm_entry.config(textvariable=self.entry_pwm)
+        self.frames['manual'].pwm_entry.bind('<Return>', self.manual__on_p_set)
+        self.frames['manual'].voltage_button.bind('<Return>', self.manual__on_v_set)
+        self.frames['manual'].voltage_button.bind('<Button-1>', self.manual__on_v_set)
+        self.frames['manual'].pwm_button.bind('<Return>', self.manual__on_p_set)
+        self.frames['manual'].pwm_button.bind('<Button-1>', self.manual__on_p_set)
 
-        
+        self.manual__init()
+
         # -------------------------------------------------------------------------------
-        # AUTO VIEW INIT
+        # AUTO VIEW WIDGETS CONFIGURATION
         # -------------------------------------------------------------------------------
-        self.frames["auto"].stop_button.config(
-            command=self.auto__on_stop,
-            state=tk.DISABLED
-        )
-        self.frames["auto"].start_button.config(command=self.auto__on_start)
-        self.frames["auto"].back_button.config(command=self.auto__on_back)
-        
+
+        self.frames['auto'].start_button.config(command=self.auto__on_start)
+        self.frames['auto'].stop_button.config(command=self.auto__on_stop)
+        self.frames['auto'].back_button.config(command=self.auto__on_back)
+
+        self.auto__init()
+
         # -------------------------------------------------------------------------------
         # Adding all frames to the grid and finally displaying Setup Frame
         # -------------------------------------------------------------------------------
+        self.frames['setup'].grid(row=0, column=0, sticky=tk.NSEW)
+        self.frames['manual'].grid(row=0, column=0, sticky=tk.NSEW)
+        self.frames['auto'].grid(row=0, column=0, sticky=tk.NSEW)
+        self.current_frame = 'setup'
+        self.show_frame(self.current_frame)
         self.deiconify()
-        self.frames["setup"].grid(row=0, column=0, sticky=tk.NSEW)
-        self.frames["manual"].grid(row=0, column=0, sticky=tk.NSEW)
-        self.frames["auto"].grid(row=0, column=0, sticky=tk.NSEW)
-        self.show_frame("setup")
-        self.deiconify()
-        
-        
+
+    # -------------------------------------------------------------------------------
+    # METHODS
+    # -------------------------------------------------------------------------------
+    
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-    
-    # -------------------------------------------------------------------------------
-    # Functions for setup view widgets
-    # -------------------------------------------------------------------------------
+
+    # Setup
     def setup__on_refresh(self):
         self.port_list = [port.device for port in comports()]
         self.frames["setup"].supply_port.set('')
@@ -140,8 +120,7 @@ class Application(tk.Tk):
                 label=choice,
                 command=tk._setit(self.frames["setup"].arduino_port, choice)
             )
-        
-    
+
     def setup__on_browse_src(self):
         self.frames["setup"].source_filename.set(fd.askopenfilename())
     
@@ -165,23 +144,43 @@ class Application(tk.Tk):
         self.show_frame(self.current_frame)
         self.frames["setup"].button_auto.config(state=tk.DISABLED)
         self.frames["setup"].button_manual.config(state=tk.DISABLED)
+
+
+    # Manual
+    def manual__init(self):
+        self.old_voltage = 0
+        self.old_pwm = 1000
+        self.slider_pwm.set(1000)
+        self.slider_voltage.set(0)
+        self.entry_pwm.set(1000)
+        self.entry_voltage.set(0)
+        self.frames['manual'].pwm_slider.config(value=1000)
+        self.frames['manual'].voltage_slider.config(value=0)
+
+        self.frames['manual'].start_button.config(state=tk.NORMAL)
+        self.frames['manual'].back_button.config(state=tk.NORMAL)
+        for widget in self.frames['manual'].pwm_frame.winfo_children():
+            widget.config(state=tk.DISABLED)
+        for widget in self.frames['manual'].voltage_frame.winfo_children():
+            widget.config(state=tk.DISABLED)
+        self.frames['manual'].stop_button.config(state=tk.DISABLED)
+        for val in self.frames["manual"].dashboard_values:
+            val.config(text = '-')
     
-    # -------------------------------------------------------------------------------
-    # Functions for manual view widgets
-    # -------------------------------------------------------------------------------
     def manual__on_start(self):
+        
         # Open power supply port, set OVP, current limit
         # turn output ON
-        self.supply = m.PowerSupply(self.data['supply_port'])
+        self.supply = PowerSupply(self.data['supply_port'])
         self.supply.setOVP(self.data['max_voltage'])
         self.supply.setCurrentLimit("{0:.3f}".format(self.data["max_current"]/1000))
         self.supply.setVoltage(0)
         self.supply.turnOutputON()
         # Open arduino port
-        self.arduino = m.Arduino(self.data['arduino_port'], self.data['num_readings'])
+        self.arduino = Arduino(self.data['arduino_port'], self.data['num_readings'])
         self.arduino.send_pwm(1000)
-        self.model = m.Model(self.data['source_file'], self.data['dest_file'], self.supply, self.arduino)
-        
+        self.model = Model(self.data['source_file'], self.data['dest_file'], self.supply, self.arduino)
+
         # Update GUI
         self.frames["manual"].back_button.config(state=tk.DISABLED)
         for widget in self.frames["manual"].pwm_frame.winfo_children():
@@ -190,8 +189,8 @@ class Application(tk.Tk):
             widget.config(state=tk.NORMAL)
         self.frames["manual"].stop_button.config(state=tk.NORMAL)
         self.frames["manual"].start_button.config(state=tk.DISABLED)
-        
-        # Start updating values
+
+        self.start_time = perf_counter()
         self._handle = self.after(1, self.update_GUI)
     
     def manual__on_stop(self):
@@ -200,23 +199,12 @@ class Application(tk.Tk):
         self.cancel_update()
         self.supply.close()
         self.arduino.close()
-        print('closed ports ...')
-        self.entry_voltage.set(0)
-        self.slider_voltage.set(0)
-        self.entry_pwm.set(1000)
-        self.slider_pwm.set(1000)
-        self.frames["manual"].back_button.config(state=tk.NORMAL)
-        self.frames["manual"].stop_button.config(state=tk.DISABLED)
-        self.frames["manual"].start_button.config(state=tk.NORMAL)
-        for widget in self.frames["manual"].pwm_frame.winfo_children():
-            widget.config(state=tk.DISABLED)
-        for widget in self.frames["manual"].voltage_frame.winfo_children():
-            widget.config(state=tk.DISABLED)
-        for val in self.frames["manual"].dashboard_values:
-            val.config(text = '-')
+
+        self.manual__init()
     
     def manual__on_back(self):
-        self.show_frame("setup")
+        self.current_frame = 'setup'
+        self.show_frame(self.current_frame)
     
     def manual__v_slider_changed(self, event):
         value = self.slider_voltage.get()
@@ -261,23 +249,28 @@ class Application(tk.Tk):
         self.slider_voltage.set(value)
         self.old_voltage = value
         self.supply.setVoltage(value)
+    
+    # Automated
+    
+    def auto__init(self):
+        self.frames["auto"].start_button.config(state=tk.NORMAL)
+        self.frames["auto"].back_button.config(state=tk.NORMAL)
+        self.frames["auto"].stop_button.config(state=tk.DISABLED)
         
-        
-    # -------------------------------------------------------------------------------
-    # Functions for auto widgets
-    # -------------------------------------------------------------------------------
+        for val in self.frames["auto"].dashboard_values:
+            val.config(text = '-')
     
     def auto__on_start(self):
-        
-        self.supply = m.PowerSupply(self.data['supply_port'])
+        # Open power supply ports
+        self.supply = PowerSupply(self.data['supply_port'])
         self.supply.setOVP(self.data['max_voltage'])
         self.supply.setCurrentLimit("{0:.3f}".format(self.data["max_current"]/1000))
         self.supply.setVoltage(self.frames['auto'].required_voltage.get())
         self.supply.turnOutputON()
         # Open arduino port
-        self.arduino = m.Arduino(self.data['arduino_port'], self.data['num_readings'])
+        self.arduino = Arduino(self.data['arduino_port'], self.data['num_readings'])
         self.arduino.send_pwm(1000)
-        self.model = m.Model(self.data['source_file'], self.data['dest_file'], self.supply, self.arduino)
+        self.model = Model(self.data['source_file'], self.data['dest_file'], self.supply, self.arduino)
         
         self.frames["auto"].stop_button.config(state=tk.NORMAL)
         self.frames["auto"].start_button.config(state=tk.DISABLED)
@@ -290,9 +283,8 @@ class Application(tk.Tk):
         self._auto_handle = True
         self.auto_thread = Thread(target=self.auto__loop)
         self.auto_thread.start()
-    
+
     def auto__on_stop(self):
-    
         self._auto_handle = None
         self.cancel_update()
         self.supply.setVoltage(0)
@@ -301,14 +293,11 @@ class Application(tk.Tk):
         self.arduino.close()
         self.frames[self.current_frame].pwm_label.config(text='')
         self.frames[self.current_frame].delay_label.config(text='')
-        
-        self.frames["auto"].back_button.config(state=tk.NORMAL)
-        self.frames["auto"].stop_button.config(state=tk.DISABLED)
-        self.frames["auto"].start_button.config(state=tk.NORMAL)
+        self.auto__init()
     
     def auto__on_back(self):
-        self.show_frame("setup")
-        
+        self.current_frame = 'setup'
+        self.show_frame(self.current_frame)
     
     def auto__loop(self):
         for i in range(len(self.model.auto_pwm)):
@@ -325,11 +314,8 @@ class Application(tk.Tk):
                 sleep(int(self.model.auto_delay[i]))
             else:
                 return
-        
-    # -------------------------------------------------------------------------------
-    # COMMON FUNCTIONS
-    # -------------------------------------------------------------------------------
-    
+
+    # Common functions
     def update_GUI(self):
         if self._handle is not None:
             self.update_thread = Thread(target=self.update_db)
@@ -355,5 +341,3 @@ class Application(tk.Tk):
         self.frames[self.current_frame].dash_power.config(text=self.model.power)
         #append to file
         self.model.append_dest_file()
-        
-        
